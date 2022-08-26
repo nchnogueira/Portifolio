cc110ddb9ae0e3feed09410684c2d46c55c4ad93
NicolasNogueira92 <NicolasNogueira_92@ufrj.br>
2022-08-26 20:03:06 -0300

    #

---
+++ Airquality.R
@@ -0,0 +1,49 @@
+#####
+
+library(tidyverse)
+library(lubridate)
+library(writexl)
+library(ggtext)
+library(extrafont)
+library(magrittr)
+library(mice)
+
+loadfonts(device = "win")
+
+#####
+
+complete(mice(data = airquality[,1:4], meth = "pmm")) -> airquality2
+
+#####
+
+airquality %>%
+  mutate(Year = 1973, .before = Month) %>%
+  unite(Date, Year:Day, sep = "-") %>%
+  mutate(Date = as.Date(Date),
+         Week = week(Date)) %>%
+  group_by(Week) %>%
+  drop_na() %>%
+  mutate(mu = mean(Temp),
+         sd = sd(Temp)) %>%
+
+  ggplot(aes(x = Week, y = mu)) +
+    geom_line(color = "#145baf", size = 2) +
+    geom_errorbar(aes(ymin = mu - sd, ymax = mu + sd, x = Week),
+                  width = 0.1, color = "#145baf") +
+    geom_point(color = "#145baf", size = 3, shape = 16) +
+    geom_hline(yintercept = 77.88235, color = "red", linetype = "twodash") +
+    labs(title = "Evolução da Temperatura em Nova York 1973",
+         x = "Semana",
+         y = "Médias Semanais de Temperatura (°F)") +
+    theme(
+      text = element_text(size = 12, family = "mono"),
+      plot.title = element_textbox_simple(size = 14),
+      plot.background = element_rect(fill = "#fefef4"),
+      plot.margin = margin(1,1,1,1, 'cm'),
+      panel.grid.major = element_line(colour = 'grey70', linetype = 'dotted'),
+      panel.grid.minor = element_line(colour = 'grey90', linetype = 'dotted'),
+      panel.background = element_rect(fill = "#fefef4"),
+      axis.line = element_line(colour = 'grey50'),
+      axis.title = element_text(colour = '#082234'),
+      axis.title.y = element_text(angle = 90, margin = margin(r = 2, unit = 'mm')),
+      axis.text = element_text())
